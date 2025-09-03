7bfe9b64b90604a694d73685d705418c3740c8b4
Alejandro Ramirez <alejandro.ramirez@amsintegra.com.mx>
2025-09-03 17:21:58 -0600

    Actualización de controladores, servicios y configuración sin log

--- src/main/java/com/creditienda/controller/ServicioProtegidoController.java
+++ src/main/java/com/creditienda/controller/ServicioProtegidoController.java
@@ -1,13 +1,14 @@
 package com.creditienda.controller;
 
-import com.creditienda.service.OAuthTokenService;
-import org.springframework.beans.factory.annotation.Value;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Map;
+import com.creditienda.service.OAuthTokenService;
 
 @RestController
 public class ServicioProtegidoController {
@@ -20,6 +21,7 @@ public class ServicioProtegidoController {
 
     private final WebClient webClient = WebClient.builder().build();
 
+    @SuppressWarnings("unchecked")
     @GetMapping("/consumir-api")
     public Map<String, Object> consumirApiProtegida() {
         String token = tokenService.obtenerToken();
